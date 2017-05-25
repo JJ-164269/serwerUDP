1ff44154231f67f1a2b70e54f284a98c23016d10
student <student@PC-218>
2017-05-25 16:07:47 +0200

    Done

---
+++ serwerUDP/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace serwerUDP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.My_port = new System.Windows.Forms.NumericUpDown();
+            this.Messages = new System.Windows.Forms.ListBox();
+            this.start = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.My_port)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // My_port
+            // 
+            this.My_port.Location = new System.Drawing.Point(208, 103);
+            this.My_port.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.My_port.Name = "My_port";
+            this.My_port.Size = new System.Drawing.Size(248, 20);
+            this.My_port.TabIndex = 0;
+            // 
+            // Messages
+            // 
+            this.Messages.FormattingEnabled = true;
+            this.Messages.Location = new System.Drawing.Point(12, 160);
+            this.Messages.Name = "Messages";
+            this.Messages.Size = new System.Drawing.Size(559, 173);
+            this.Messages.TabIndex = 1;
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(208, 363);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(144, 45);
+            this.start.TabIndex = 2;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 444);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.Messages);
+            this.Controls.Add(this.My_port);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.My_port)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown My_port;
+        private System.Windows.Forms.ListBox Messages;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ serwerUDP/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+namespace serwerUDP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            int port = Convert.ToInt32(My_port.Value);
+            IPEndPoint zdalnyIP = new IPEndPoint(IPAddress.Any, 0);
+
+            try
+            {
+                UdpClient serwer = new UdpClient(port);
+                Byte[] odczyt = serwer.Receive(ref zdalnyIP);
+                string dane = Encoding.ASCII.GetString(odczyt);
+                Messages.Items.Add(dane);
+                serwer.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Błąd!");
+            }
+        }
+    }
+}
